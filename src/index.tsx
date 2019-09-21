70f0f88030a82006ecb4df7374083cc3ff93b736
Aleš Chromec <chromec.ales@gmail.com>
2019-09-21 11:03:16 +0200

    Textbox: Export in index

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,6 @@ import Input from './Input';
 import SelectBox from './SelectBox'
 import SelectPicker from './SelectPicker'
 import Checkbox from './Checkbox'
+import TextArea from "./TextArea"
 
-export { Input, SelectBox, SelectPicker, Checkbox };
+export { Input, SelectBox, SelectPicker, Checkbox, TextArea };
