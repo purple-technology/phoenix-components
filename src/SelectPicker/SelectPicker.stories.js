7a8c1ecc907b6d6655caa4eb5ef00d2acf76e9af
Aleš Chromec <chromec.ales@gmail.com>
2019-12-19 09:52:37 +0100

    Fix select picker grid on screens under 768px

--- src/SelectPicker/SelectPicker.stories.js
+++ src/SelectPicker/SelectPicker.stories.js
@@ -1,52 +1,57 @@
-import React from "react"
-import { storiesOf } from "@storybook/react"
-import SelectPicker from "./index"
-import { withKnobs, text } from "@storybook/addon-knobs";
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import SelectPicker from './index'
+import { withKnobs, text } from '@storybook/addon-knobs'
 
-const DEMO_IMAGE = "https://image.flaticon.com/icons/svg/566/566944.svg"
-const OPTIONS = [{ value: "secondary", image: DEMO_IMAGE, label: "Secondary education" }, { value: "primary", image: DEMO_IMAGE, label: "Primary education" }]
-const OPTIONS_NO_IMAGE = [{ value: "secondary", label: "Secondary education" }, { value: "primary", label: "Primary education" }]
+const DEMO_IMAGE = 'https://image.flaticon.com/icons/svg/566/566944.svg'
+const OPTIONS = [
+  { value: 'secondary', image: DEMO_IMAGE, label: 'Secondary education' },
+  { value: 'primary', image: DEMO_IMAGE, label: 'Primary education' },
+  { value: 'university', image: DEMO_IMAGE, label: 'University education' }
+]
+const OPTIONS_NO_IMAGE = [
+  { value: 'secondary', label: 'Secondary education' },
+  { value: 'primary', label: 'Primary education' },
+  { value: 'university', label: 'university education' }
+]
 
 storiesOf('Select picker', module)
-    .addParameters({
-        options: {
-            showPanel: true,
-            showNav: true,
-            panelPosition: 'bottom'
-        },
-        info: {
-            inline: true, header: false
-        },
-    })
-    .addDecorator(withKnobs)
-    .addDecorator(storyFn =>
-        <div style={{ margin: "50px auto" }}>
-            {storyFn()}
-        </div>
+  .addParameters({
+    options: {
+      showPanel: true,
+      showNav: true,
+      panelPosition: 'bottom'
+    },
+    info: {
+      inline: true,
+      header: false
+    }
+  })
+  .addDecorator(withKnobs)
+  .addDecorator(storyFn => (
+    <div style={{ margin: '50px auto' }}>{storyFn()}</div>
+  ))
+  .add('Default', () => {
+    const ErrorKnob = text('Error', '')
+    return (
+      <SelectPicker
+        label="Select something"
+        value="secondary"
+        error={ErrorKnob}
+        onChange={e => console.log(e)}
+        options={OPTIONS_NO_IMAGE}
+      />
     )
-    .add('Default', () => {
-        const ErrorKnob = text("Error", "")
-        return (
-            <SelectPicker
-                label="Select something"
-                value="secondary"
-                error={ErrorKnob}
-                onChange={(e) => console.log(e)}
-                options={OPTIONS_NO_IMAGE}
-            />
-        )
-    })
-    .add('With images', () => {
-        const ErrorKnob = text("Error", "")
-        return (
-            <SelectPicker
-                label="Select something!"
-                value="secondary"
-                error={ErrorKnob}
-                onChange={(e) => console.log(e)}
-                options={OPTIONS}
-            />
-        )
-    })
-
-
+  })
+  .add('With images', () => {
+    const ErrorKnob = text('Error', '')
+    return (
+      <SelectPicker
+        label="Select something!"
+        value="secondary"
+        error={ErrorKnob}
+        onChange={e => console.log(e)}
+        options={OPTIONS}
+      />
+    )
+  })
--- src/SelectPicker/SelectPickerStyles.tsx
+++ src/SelectPicker/SelectPickerStyles.tsx
@@ -1,79 +1,97 @@
 import styled from 'styled-components'
 
 export const SelectPickerWrapper = styled.div`
-    font-family: 'Roboto', sans-serif;
-`;
+  font-family: 'Roboto', sans-serif;
+`
 
 export const SelectWrapper = styled.div<any>`
-    display: grid;
-    grid-template-columns: ${({ optionsLength }) => `repeat(${optionsLength}, minmax(100px, 200px))`};
-    grid-column-gap: 25px;
+  display: grid;
+  grid-template-columns: ${({ optionsLength }) =>
+    `repeat(${optionsLength}, minmax(100px, 200px))`};
+  grid-column-gap: 25px;
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    grid-row-gap: 20px;
+    max-width: 100%;
+  }
 `
 
 export const OptionDecription = styled.div`
-    font-size: 12px;
-    color: rgba(0,0,0,0.6);
-    margin-top: 6px;
+  font-size: 12px;
+  color: rgba(0, 0, 0, 0.6);
+  margin-top: 6px;
 `
 
 export const SingleCard = styled.div<any>`
-    display: flex;
-    position: relative;
-    min-height: ${({ withImage }) => withImage ? "80px" : "auto"};
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 14px 0;
-    text-align: center;
-    border: 1px solid;
-    background: #fff;
-    color: rgba(0, 0, 0, 0.8);
-    font-size: 14px;
-    border-color: ${({ checked }: any) =>
-        checked ? "#562878" : 'rgba(95,95,95,0.3)'};
-    box-shadow: ${({ checked }: any) =>
-        checked ? ' 0px 0px 11px 2px rgba(82, 41, 116, 0.1);' : 'none'};
-    font-weight: 500;
-    border-radius: 4px;
-    transition: border 0.3s;
-    cursor: ${({ checked, multiSelect }: any) => (checked && !multiSelect ? 'default' : 'pointer')};
-    user-select: none;
+  display: flex;
+  position: relative;
+  min-height: ${({ withImage }) => (withImage ? '80px' : 'auto')};
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding: 14px 0;
+  text-align: center;
+  border: 1px solid;
+  background: #fff;
+  color: rgba(0, 0, 0, 0.8);
+  font-size: 14px;
+  border-color: ${({ checked }: any) =>
+    checked ? '#562878' : 'rgba(95,95,95,0.3)'};
+  box-shadow: ${({ checked }: any) =>
+    checked ? ' 0px 0px 11px 2px rgba(82, 41, 116, 0.1);' : 'none'};
+  font-weight: 500;
+  border-radius: 4px;
+  transition: border 0.3s;
+  cursor: ${({ checked, multiSelect }: any) =>
+    checked && !multiSelect ? 'default' : 'pointer'};
+  user-select: none;
 
-    &:hover {
-        border-color: #562878;
-    }
+  &:hover {
+    border-color: #562878;
+  }
+
+  @media (max-width: 768px) {
+    margin: 0 1.3rem;
+    flex-direction: row;
+    padding: 7px 0;
+    min-height: 60px;
+  }
 `
 
 export const CardImage = styled.img`
-    max-width: 40px;
-    margin-bottom: 10px;
-`;
+  max-width: 40px;
+  margin-bottom: 10px;
+  @media (max-width: 768px) {
+    margin: 0 10px 0 0;
+    max-width: 30px;
+  }
+`
 
 export const PickerLabel = styled.label`
-    display: block;
-    margin: 0 auto 30px;
-    text-align: center;
-    line-height: 20px;
-    font-size: 14px;
-    color: rgba(0, 0, 0, 0.7);
-`;
+  display: block;
+  margin: 0 auto 30px;
+  text-align: center;
+  line-height: 20px;
+  font-size: 14px;
+  color: rgba(0, 0, 0, 0.7);
+`
 
 export const CheckMark = styled.div`
-    position: absolute;
-    width: 32px;
-    height: 32px;
-    background: #562878;
-    border-radius: 2px;
-    top: -15px;
-    right: -15px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`;
+  position: absolute;
+  width: 32px;
+  height: 32px;
+  background: #562878;
+  border-radius: 2px;
+  top: -15px;
+  right: -15px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
 
 export const Error = styled.div`
   color: rgba(204, 0, 0, 0.82);
   padding: 5px 0;
   font-size: 13px;
   margin-top: 5px;
-`;
+`
