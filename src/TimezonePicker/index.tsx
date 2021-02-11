36aa6d8a861295cc845036e9a9edad80a696126f
Alex Drozd <drozdster@gmail.com>
2021-02-11 14:17:39 +0100

    small refactors into util functions

--- src/TimezonePicker/index.tsx
+++ src/TimezonePicker/index.tsx
@@ -2,39 +2,14 @@ import React from 'react'
 import moment from 'moment-timezone'
 
 import SelectBox, { Option, SelectProps } from '../SelectBox'
+import { buildOptions, sortTimezones } from './utils'
 
 type TimezonePickerProps = Omit<SelectProps, 'options'>
 
 const TimezonePicker: React.FC<TimezonePickerProps> = props => {
   const timeZones = moment.tz.names()
-
-  const offsetSorted = timeZones.sort((a, b) => {
-    const aOffset = moment.tz(a).utcOffset()
-    const bOffset = moment.tz(b).utcOffset()
-    // sort cities with the same offset alphabetically
-    if (aOffset === bOffset) {
-      if (a < b) {
-        return -1
-      }
-      if (a > b) {
-        return 1
-      }
-      return 0
-    }
-    return aOffset - bOffset
-  })
-
-  const options: Option[] = offsetSorted.map((timezone, i) => {
-    const noContinentTimezone = timezone.includes('/')
-      ? timezone.split('/')[1]
-      : timezone
-    const cleanTimezome = noContinentTimezone.replace('_', ' ')
-    const offset = moment.tz(timezone).format('Z')
-    return {
-      value: timezone,
-      label: `(GMT${offset}) ${cleanTimezome}`
-    }
-  })
+  const offsetSorted = sortTimezones(timeZones)
+  const options = buildOptions(offsetSorted)
   return <SelectBox options={options} {...props} />
 }
 
---
+++ src/TimezonePicker/utils.ts
@@ -0,0 +1,35 @@
+import moment from 'moment-timezone'
+
+import { Option } from '../SelectBox'
+
+export const sortTimezones = (timeZones: string[]) => {
+  return timeZones.sort((a, b) => {
+    const aOffset = moment.tz(a).utcOffset()
+    const bOffset = moment.tz(b).utcOffset()
+    // sort cities with the same offset alphabetically
+    if (aOffset === bOffset) {
+      if (a < b) {
+        return -1
+      }
+      if (a > b) {
+        return 1
+      }
+      return 0
+    }
+    return aOffset - bOffset
+  })
+}
+
+export const buildOptions = (timezones: string[]): Option[] => {
+  return timezones.map((timezone, i) => {
+    const noContinentTimezone = timezone.includes('/')
+      ? timezone.split('/')[1]
+      : timezone
+    const cleanTimezome = noContinentTimezone.replace('_', ' ')
+    const offset = moment.tz(timezone).format('Z')
+    return {
+      value: timezone,
+      label: `(GMT${offset}) ${cleanTimezome}`
+    }
+  })
+}
