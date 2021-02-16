b4e7697d7c340cb7962ba538384b099e05274bc4
Alex Drozd <drozdster@gmail.com>
2021-02-16 10:41:48 +0100

    exposing getOptions to help with initial value

--- src/TimezonePicker/index.tsx
+++ src/TimezonePicker/index.tsx
@@ -6,13 +6,18 @@ import { buildOptions, sortTimezones } from './utils'
 
 type TimezonePickerProps = Omit<SelectProps, 'options'>
 
-const TimezonePicker: React.FC<TimezonePickerProps> = props => {
+export const getOptions = () => {
   const allTimezones = getAllTimezones()
   const timezones = Object.keys(allTimezones).map(
     (timezone, i) => allTimezones[timezone]
   )
   const offsetSorted = sortTimezones(timezones)
   const options = buildOptions(offsetSorted)
+  return options
+}
+
+const TimezonePicker: React.FC<TimezonePickerProps> = props => {
+  const options = getOptions()
   return <SelectBox options={options} {...props} />
 }
 
