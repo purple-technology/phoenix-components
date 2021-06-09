ec07af8fe869765e0ec1ce850d8ec32d8c4ba814
Adam Štěpánek <stepanek.adam@gmail.com>
2021-06-09 12:15:13 +0200

    chore: editorconfig, gitignore and storybook config file update

--- .storybook/purpleTheme.js
+++ .storybook/purpleTheme.js
@@ -5,6 +5,6 @@ import logo from '../public/phoenix-components.png'
 export default create({
 	base: 'light',
 	brandTitle: 'Phoenix Components',
-	brandUrl: 'https://purple-technology.com',
+	brandUrl: 'https://purple-technology.github.io/phoenix-components/',
 	brandImage: logo
 })
