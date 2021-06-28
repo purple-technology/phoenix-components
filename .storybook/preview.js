c5eec2cf82f7d315040a229c767b3aa426ea5390
Adam Štěpánek <stepanek.adam@gmail.com>
2021-06-28 16:39:33 +0200

    style: fix linter errors

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import { ThemeProvider } from 'styled-components'
 
-import theme from '../src/theme'
 import { GlobalStyles } from '../src/globalStyles'
+import theme from '../src/theme'
 
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
@@ -18,7 +18,7 @@ export const parameters = {
 			a[1].kind === b[1].kind
 				? 0
 				: a[1].id.localeCompare(b[1].id, undefined, { numeric: true })
-	},
+	}
 	// viewMode: 'docs'
 }
 
