0640c732d26a3da1cbd9ab60ead2b4e910bc2736
Adam Štěpánek <stepanek.adam@gmail.com>
2021-06-29 12:20:35 +0200

    feat: tag component - optional prop colorTheme

--- src/components/Tag/index.tsx
+++ src/components/Tag/index.tsx
@@ -4,10 +4,13 @@ import { ColorTheme } from '../../types/ColorTheme'
 import { StyledTag } from './TagStyles'
 
 export interface TagProps {
-	colorTheme: ColorTheme
+	colorTheme?: ColorTheme
 	className?: string
 }
 
-export const Tag: React.FC<TagProps> = ({ children, ...props }) => {
-	return <StyledTag {...props}>{children}</StyledTag>
+export const Tag: React.FC<TagProps> = ({
+	colorTheme = 'primary',
+	...props
+}) => {
+	return <StyledTag colorTheme={colorTheme} {...props} />
 }
