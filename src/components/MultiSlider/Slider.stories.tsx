f86ca5b1852527afe0f00ab38ab90b2213f532d8
Adam Štěpánek <stepanek.adam@gmail.com>
2021-08-16 08:56:10 +0200

    feat: component Slider split into Slider and MultiSlider

---
+++ src/components/MultiSlider/Slider.stories.tsx
@@ -0,0 +1,101 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+
+import { Spacer } from '../Spacer'
+import { TextInput } from '../TextInput'
+import { MultiSlider as MultiSliderComponent, MultiSliderValue } from './index'
+
+export default {
+	title: 'components / MultiSlider',
+	component: MultiSliderComponent
+} as ComponentMeta<typeof MultiSliderComponent>
+
+export const Range: ComponentStory<typeof MultiSliderComponent> = (args) => {
+	const [value, setValue] = useState<MultiSliderValue>([60, 80])
+
+	return (
+		<MultiSliderComponent
+			{...args}
+			value={value}
+			onChange={(value): void => setValue(value)}
+			range={{
+				min: 0,
+				max: 100
+			}}
+		/>
+	)
+}
+
+const FlexRow = styled.div`
+	display: flex;
+`
+
+export const RangeWithInputs: ComponentStory<typeof MultiSliderComponent> = (
+	args
+) => {
+	const [inputValue, setInputValue] = useState<MultiSliderValue>([60, 80])
+	const [sliderValue, setSliderValue] = useState<MultiSliderValue>([60, 80])
+
+	const startValue = inputValue[0]
+	const endValue = inputValue[1]
+
+	return (
+		<>
+			<FlexRow>
+				<TextInput
+					value={startValue.toString()}
+					onChange={(e): void =>
+						setInputValue([e.currentTarget.value, endValue])
+					}
+					onBlur={(e): void =>
+						setSliderValue([e.currentTarget.value, endValue])
+					}
+				/>
+				<Spacer w="s" />
+				<TextInput
+					value={endValue}
+					onChange={(e): void =>
+						setInputValue([startValue, e.currentTarget.value])
+					}
+					onBlur={(e): void =>
+						setSliderValue([startValue, e.currentTarget.value])
+					}
+				/>
+			</FlexRow>
+			<Spacer h="s" />
+			<MultiSliderComponent
+				{...args}
+				value={sliderValue}
+				onChange={(value): void => {
+					setSliderValue(value)
+					setInputValue(value)
+				}}
+				range={{
+					min: 0,
+					max: 100
+				}}
+			/>
+		</>
+	)
+}
+RangeWithInputs.storyName = 'RangeWithInputs'
+
+export const MultiSlider: ComponentStory<typeof MultiSliderComponent> = (
+	args
+) => {
+	const [value, setValue] = useState<MultiSliderValue>([25, 50, 75])
+
+	return (
+		<MultiSliderComponent
+			{...args}
+			value={value}
+			onChange={(value): void => setValue(value)}
+			range={{
+				min: 0,
+				max: 100
+			}}
+		/>
+	)
+}
+MultiSlider.storyName = 'MultiSlider'
---
+++ src/components/common/Slider/useSlider.tsx
@@ -0,0 +1,42 @@
+import isEqual from 'lodash.isequal'
+import noUiSlider, { API } from 'nouislider'
+import React, { useEffect, useRef } from 'react'
+
+import { CommonSliderProps } from '.'
+
+export function useSlider<T extends string | number | (string | number)[]>(
+	value: T,
+	sliderProps: CommonSliderProps,
+	prevValue?: T
+): [React.MutableRefObject<API | undefined>, React.RefObject<HTMLDivElement>] {
+	const sliderRef = useRef<HTMLDivElement>(null)
+	const slider = useRef<API>()
+
+	useEffect(() => {
+		if (!sliderRef || !sliderRef.current) return
+
+		//For 1 value highlight lower part of slider, for 2 values (range) highlight in between
+		const connect =
+			sliderProps.connect ?? ['number', 'string'].includes(typeof value)
+				? 'lower'
+				: Array.isArray(value) && value.length === 2
+				? [false, true, false]
+				: undefined
+
+		slider.current = noUiSlider.create(sliderRef.current, {
+			start: value,
+			range: sliderProps.range,
+			step: sliderProps.step,
+			connect,
+			animate: false
+		})
+	}, [sliderRef])
+
+	useEffect(() => {
+		if (!isEqual(value, prevValue)) {
+			slider.current?.set(value)
+		}
+	}, [value])
+
+	return [slider, sliderRef]
+}
