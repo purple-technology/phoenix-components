f438c9df238da3346933dc900ee3ce77f6459c3f
Christopher Horobin <chorobin>
2021-10-11 14:47:12 +0200

    fix(Collapsible): set height to auto when expanded

--- src/components/Collapsible/CollapsibleHooks.tsx
+++ src/components/Collapsible/CollapsibleHooks.tsx
@@ -8,30 +8,54 @@ interface UseCollapsible {
 }
 
 export const useCollapsible = (collapsed: boolean): UseCollapsible => {
-	const [clientHeight, setClientHeight] = React.useState<string>('auto')
+	const [scrollHeight, setScrollHeight] = React.useState<string>()
 	const [visibility, setVisibility] = React.useState(
 		collapsed ? 'hidden' : 'visible'
 	)
+	const [height, setHeight] = React.useState(collapsed ? '0px' : 'auto')
+	const hasMountedRef = React.useRef(false)
+	const animationFrameRef = React.useRef<number>()
 
 	React.useEffect(() => {
-		if (!collapsed) {
-			setVisibility('visible')
+		if (scrollHeight == null) return
+
+		if (!hasMountedRef.current) {
+			hasMountedRef.current = true
+			return
 		}
-	}, [collapsed])
+
+		setVisibility('visible')
+		setHeight(scrollHeight)
+	}, [collapsed, scrollHeight])
+
+	React.useEffect(() => {
+		if (scrollHeight == null) return
+
+		if (collapsed && height === scrollHeight) {
+			animationFrameRef.current = requestAnimationFrame(() => {
+				setHeight('0px')
+			})
+		}
+
+		return (): void => {
+			if (animationFrameRef.current == null) return
+			cancelAnimationFrame(animationFrameRef.current)
+		}
+	}, [collapsed, height, scrollHeight])
 
 	const ref = React.useCallback((ref: HTMLDivElement | null) => {
 		if (ref != null) {
-			setClientHeight(`${ref.clientHeight}px`)
+			setScrollHeight(`${ref.scrollHeight}px`)
 		}
 	}, [])
 
 	const onTransitionEnd = (): void => {
 		if (collapsed) {
 			setVisibility('hidden')
+		} else {
+			setHeight('auto')
 		}
 	}
-
-	const height = collapsed ? '0px' : clientHeight
 
 	return { ref, height, onTransitionEnd, visibility }
 }
