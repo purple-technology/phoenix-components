558adf7c3eec13e0c6f10a6efb2f9bc86dd2e1a6
Christopher Horobin <chorobin>
2021-10-11 15:08:56 +0200

    fix(CollapsibleCard): highlight button when header is hovered over

--- src/components/CollapsibleCard/CollapsibleCardStyles.tsx
+++ src/components/CollapsibleCard/CollapsibleCardStyles.tsx
@@ -4,4 +4,8 @@ export const HeadingContainerStyle = styled.header`
 	display: flex;
 	align-items: center;
 	cursor: pointer;
+	:hover > button {
+		background-color: ${(props): string =>
+			props.theme.$pc.colors.neutral.lightHoverBackground};
+	}
 `
