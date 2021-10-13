cc8abec4ce913587278e010303a4f40f0c83c06b
Christopher Horobin <chorobin>
2021-10-13 14:25:46 +0200

    fix(Collapsible): make overflow visible when expanded

--- src/components/Collapsible/CollapsibleStyles.tsx
+++ src/components/Collapsible/CollapsibleStyles.tsx
@@ -3,10 +3,11 @@ import styled from 'styled-components'
 export const CollapsibleStyle = styled.div<{
 	$height: string
 	$visibility: string
+	$overflow: string
 }>`
 	transition: height ${(props): string => props.theme.$pc.transitionDuration}
 		ease-out;
-	overflow: hidden;
 	height: ${(props): string => props.$height};
 	visibility: ${(props): string => props.$visibility};
+	overflow: ${(props): string => props.$overflow};
 `
