38686389dce98a4930cb31b3af148ee314460198
Adam Štěpánek <stepanek.adam@gmail.com>
2021-10-14 10:10:14 +0200

    feat(TextAlign): margin and padding props added

--- src/components/TextAlign/index.tsx
+++ src/components/TextAlign/index.tsx
@@ -2,8 +2,14 @@ import React from 'react'
 import styled from 'styled-components'
 
 import { GenericComponentProps } from '../../interfaces/GenericComponentProps'
+import { MarginProps } from '../common/Spacing/MarginProps'
+import { PaddingProps } from '../common/Spacing/PaddingProps'
+import { marginCss, paddingCss } from '../common/Spacing/SpacingStyles'
 
-export interface TextAlignProps extends GenericComponentProps {
+export interface TextAlignProps
+	extends GenericComponentProps,
+		MarginProps,
+		PaddingProps {
 	center?: boolean
 	right?: boolean
 	justify?: boolean
@@ -16,6 +22,9 @@ const StyledTextAlign = styled.div<TextAlignProps>`
 		if (justify) return 'justify'
 		return 'start'
 	}};
+
+	${paddingCss}
+	${marginCss}
 `
 
 export const TextAlign: React.FC<TextAlignProps> = ({
