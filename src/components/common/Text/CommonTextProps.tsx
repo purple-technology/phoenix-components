41c6246f9945e4f5266bb1a978734442278888fd
Adam Štěpánek <stepanek.adam@gmail.com>
2021-10-18 15:38:15 +0200

    feat: textAlign prop added to text components

--- src/components/common/Text/CommonTextProps.tsx
+++ src/components/common/Text/CommonTextProps.tsx
@@ -2,13 +2,19 @@ import { HTMLAttributes } from 'react'
 
 import { GenericComponentProps } from '../../../interfaces/GenericComponentProps'
 import { ColorTheme } from '../../../types/ColorTheme'
+import { TextAlignment } from '../../../types/TextAlignment'
 import { TextColor } from '../../../types/TextColor'
 
 // Common interface for components Text, Paragraph and Heading
 export interface CommonTextProps<T>
 	extends HTMLAttributes<T>,
-		GenericComponentProps {
+		GenericComponentProps,
+		TextAlignProp {
 	colorTheme?: ColorTheme
 	color?: TextColor
 	bold?: boolean
+}
+
+export interface TextAlignProp {
+	textAlign?: TextAlignment
 }
---
+++ src/types/TextAlignment.tsx
@@ -0,0 +1,3 @@
+export const TextAlignment = ['left', 'center', 'right', 'justify'] as const
+
+export type TextAlignment = typeof TextAlignment[number]
