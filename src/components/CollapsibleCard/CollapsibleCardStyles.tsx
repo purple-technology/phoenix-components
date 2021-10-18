4c5ad5745ea1d70a9e1750004bde7f44a6e4041f
Adam Štěpánek <stepanek.adam@gmail.com>
2021-10-18 09:38:45 +0200

    feat(CollapsibleCard): rightContent prop added

--- src/components/CollapsibleCard/CollapsibleCardStyles.tsx
+++ src/components/CollapsibleCard/CollapsibleCardStyles.tsx
@@ -1,11 +1,19 @@
 import styled from 'styled-components'
 
 import { Button } from '../Button'
+import { Spacer } from '../Spacer'
 
-export const HeadingContainerStyle = styled.header`
+export const Header = styled.header`
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	flex-wrap: wrap;
+`
+export const HeadingContainer = styled(Spacer)`
 	display: flex;
 	align-items: center;
 	cursor: pointer;
+
 	:hover > button {
 		background-color: ${(props): string =>
 			props.theme.$pc.colors.neutral.lightHoverBackground};
@@ -13,6 +21,7 @@ export const HeadingContainerStyle = styled.header`
 `
 
 export const ButtonStyle = styled(Button)<{ collapsed: boolean }>`
+	padding: 6px 9px;
 	svg {
 		transition: transform
 			${(props): string => props.theme.$pc.transitionDuration} ease-out;
