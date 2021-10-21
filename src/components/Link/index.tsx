557a8c126665f069eaeaf99de8adf0bd9733e366
Adam Štěpánek <stepanek.adam@gmail.com>
2021-10-21 15:36:16 +0200

    feat(Link): new props

--- src/components/Link/index.tsx
+++ src/components/Link/index.tsx
@@ -1,16 +1,26 @@
 import React, { forwardRef } from 'react'
 
 import { GenericComponentProps } from '../../interfaces/GenericComponentProps'
+import { ColorTheme } from '../../types/ColorTheme'
 import { IconAlignment } from '../../types/IconAlignment'
 import { IconType } from '../../types/IconType'
+import { TextColor } from '../../types/TextColor'
+import { MarginProps } from '../common/Spacing/MarginProps'
+import { PaddingProps } from '../common/Spacing/PaddingProps'
 import LinkIcon from './LinkIcon'
-import { StyledLink } from './LinkStyles'
+import { LinkText, StyledLink } from './LinkStyles'
 
 export interface LinkProps
 	extends React.AnchorHTMLAttributes<HTMLAnchorElement>,
-		GenericComponentProps {
+		GenericComponentProps,
+		MarginProps,
+		PaddingProps {
 	icon?: IconType
 	iconAlignment?: IconAlignment
+	colorTheme?: ColorTheme
+	color?: TextColor
+	bold?: boolean
+	noUnderline?: boolean
 }
 
 /**
@@ -19,19 +29,21 @@ export interface LinkProps
 export const Link: React.ForwardRefExoticComponent<
 	React.PropsWithoutRef<LinkProps> & React.RefAttributes<HTMLAnchorElement>
 > = forwardRef(function Link(
-	{ testId = 'Link', children, icon, iconAlignment = 'left', ...props },
+	{ testId = 'Link', children, icon, iconAlignment = 'left', color, ...props },
 	ref
 ) {
 	return (
-		<StyledLink ref={ref} data-testid={testId} {...props}>
+		<StyledLink ref={ref} data-testid={testId} $color={color} {...props}>
 			{iconAlignment === 'left' && (
-				<LinkIcon icon={icon} iconAlignment={iconAlignment} />
+				<LinkIcon icon={icon} color={color} colorTheme={props.colorTheme} />
 			)}
 
-			{children}
+			<LinkText icon={!!icon} iconAlignment={iconAlignment}>
+				{children}
+			</LinkText>
 
 			{iconAlignment === 'right' && (
-				<LinkIcon icon={icon} iconAlignment={iconAlignment} />
+				<LinkIcon icon={icon} color={color} colorTheme={props.colorTheme} />
 			)}
 		</StyledLink>
 	)
