169fbd2390107fe08955c9e2d3a1a223615c6910
Adam Štěpánek <stepanek.adam@gmail.com>
2021-11-10 11:08:30 +0100

    feat(Modal): new component Modal

---
+++ src/components/Modal/Modal.stories.tsx
@@ -0,0 +1,45 @@
+import { ComponentMeta, ComponentStory } from '@storybook/react'
+import React, { useState } from 'react'
+
+import { Button } from '../Button'
+import { Heading } from '../Heading'
+import { Paragraph } from '../Paragraph'
+import { Modal as ModalComponent } from './index'
+
+export default {
+	title: 'components / Modal',
+	component: ModalComponent
+} as ComponentMeta<typeof ModalComponent>
+
+export const Modal: ComponentStory<typeof ModalComponent> = (args) => {
+	const [open, setOpen] = useState(false)
+
+	return (
+		<>
+			<Button onClick={(): void => setOpen(true)}>Open modal</Button>
+			<ModalComponent
+				{...args}
+				open={open}
+				onClose={(): void => setOpen(false)}
+			>
+				<Heading>Hello modal!</Heading>
+				<Paragraph mt="s">
+					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis
+					mauris non metus varius egestas. Suspendisse condimentum ligula non
+					egestas placerat. Curabitur mollis lectus nisi, sit amet blandit enim
+					ornare quis. Sed bibendum et mi sed elementum. Morbi finibus
+					condimentum sem, quis euismod nunc tempus vitae. Aliquam dignissim
+					sapien neque.
+				</Paragraph>
+				<Paragraph mt="s">
+					Praesent tincidunt id quam in eleifend. Vivamus rutrum felis consequat
+					odio mattis mollis id convallis arcu. Nam nulla dui, fermentum eget
+					tellus at, vehicula euismod nunc. Phasellus egestas mi metus, eu
+					sagittis elit hendrerit a. Morbi pharetra ante eget suscipit
+					fermentum. Mauris fermentum lorem magna, nec facilisis sapien
+					dignissim sit amet.
+				</Paragraph>
+			</ModalComponent>
+		</>
+	)
+}
---
+++ src/components/Modal/ModalStyles.tsx
@@ -0,0 +1,70 @@
+import styled, { css, FlattenSimpleInterpolation } from 'styled-components'
+
+import { ComponentSize } from '../../types/ComponentSize'
+import { Button } from '../Button'
+import { marginCss, paddingCss } from '../common/Spacing/SpacingStyles'
+
+interface OverlayProps {
+	visible: boolean
+	animate: boolean
+}
+
+export const Overlay = styled.div<OverlayProps>`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100vw;
+	height: 100vh;
+	overflow: auto;
+	z-index: ${({ theme }): number => theme.$pc.modal.zIndex};
+	background: ${({ theme }): string => theme.$pc.modal.overlayBackground};
+	opacity: ${({ visible }): number => (visible ? 1 : 0)};
+	${({ theme, animate }): FlattenSimpleInterpolation =>
+		animate
+			? css`
+					transition: opacity ${theme.$pc.transitionDuration};
+			  `
+			: css``}
+`
+
+export const Center = styled.div<{ center: boolean }>`
+	display: flex;
+	justify-content: center;
+	align-items: ${({ center }): string => (center ? 'center' : 'flex-start')};
+	min-height: 100%;
+`
+
+interface WindowProps {
+	visible: boolean
+	animate: boolean
+	$size: ComponentSize
+}
+
+export const Window = styled.div<WindowProps>`
+	border-radius: ${({ theme }): string => theme.$pc.borderRadius.large};
+	box-shadow: ${({ theme }): string => theme.$pc.modal.windowShadow};
+	background: white;
+	position: relative;
+	${({ theme, animate }): FlattenSimpleInterpolation =>
+		animate
+			? css`
+					transition: transform ${theme.$pc.transitionDuration};
+			  `
+			: css``}
+	transform: ${({ visible }): string =>
+		visible ? 'translateY(0)' : 'translateY(-20px)'};
+	width: 100%;
+	min-height: 58px;
+	max-width: ${({ theme, $size }): number => theme.$pc.modal.size[$size]}px;
+
+	${marginCss}
+	${paddingCss}
+`
+
+export const CloseButton = styled(Button)`
+	position: absolute;
+	top: 1rem;
+	right: 1rem;
+	padding: 4px;
+	min-height: 30px;
+`
