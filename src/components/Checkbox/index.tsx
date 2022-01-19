139761be4348f45b7a3882f0073241ea450aa795
Adam Štěpánek <stepanek.adam@gmail.com>
2022-01-19 09:42:20 +0100

    feat: checkbox and radio components - error state

--- src/components/Checkbox/index.tsx
+++ src/components/Checkbox/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 import { CheckboxRadioCommonProps } from '../common/CheckboxRadio'
 import CheckboxRadio from '../common/CheckboxRadio'
+import FormControlWarningError from '../common/FormControlWarningError'
 import { StyledCheckbox } from './CheckboxStyles'
 
 export type CheckboxProps = CheckboxRadioCommonProps
@@ -13,17 +14,24 @@ export const Checkbox: React.VoidFunctionComponent<CheckboxProps> = ({
 	size = 'medium',
 	colorTheme = 'primary',
 	className,
+	warning,
+	error,
 	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	RTL,
 	testId = 'Checkbox',
 	...props
 }) => (
-	<StyledCheckbox
-		className={className}
-		colorTheme={colorTheme}
-		size={size}
-		data-testid={testId}
-	>
-		<CheckboxRadio type="checkbox" {...props} />
-	</StyledCheckbox>
+	<>
+		<StyledCheckbox
+			className={className}
+			colorTheme={colorTheme}
+			size={size}
+			data-testid={testId}
+			warning={!!warning}
+			error={!!error}
+		>
+			<CheckboxRadio type="checkbox" {...props} />
+		</StyledCheckbox>
+		<FormControlWarningError warning={warning} error={error} />
+	</>
 )
--- src/components/common/CheckboxRadio/stories.tsx
+++ src/components/common/CheckboxRadio/stories.tsx
@@ -1,6 +1,17 @@
 import { ButtonColorTheme } from '../../../types/ColorTheme'
 
 export const argTypes = {
+	/** Prop error was by default JSON but we need text. */
+	error: {
+		control: 'text'
+	},
+	/** Prop warning was by default JSON but we need text. */
+	warning: {
+		control: 'text'
+	},
+	label: {
+		control: 'text'
+	},
 	size: {
 		options: ['medium', 'large'],
 		defaultValue: 'medium'
