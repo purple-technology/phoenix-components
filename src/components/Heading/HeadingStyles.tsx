e70eb3a8cf91ad44af5592bb382e7a898102a42b
Adam Štěpánek <stepanek.adam@gmail.com>
2023-01-26 17:19:03 +0100

    fix(Heading): sizing

--- src/components/Heading/HeadingStyles.tsx
+++ src/components/Heading/HeadingStyles.tsx
@@ -1,9 +1,9 @@
 import styled from 'styled-components'
 
-import { getTextColor } from '../../tokens/helpers'
+import { getLineHeightUnitless, getTextColor } from '../../tokens/helpers'
 import { Color } from '../../types/Color'
 import { CSSValue } from '../../types/CSSValue'
-import { Sizing } from '../../types/Sizing'
+import { isSizing, Sizing } from '../../types/Sizing'
 import { marginCss, paddingCss } from '../common/Spacing/SpacingStyles'
 import { TextAlignProp } from '../common/Text/CommonTextProps'
 import { textAlignCss } from '../common/Text/TextStyles'
@@ -28,15 +28,16 @@ export const StyledHeading = styled.h1<StyledHeadingProps>`
 		size
 			? typeof size === 'number'
 				? `${size}px`
-				: size in Sizing
-				? theme.tokens.heading.fontSize[size as Sizing]
+				: isSizing(size)
+				? theme.tokens.heading.fontSize[size]
 				: size
 			: theme.tokens.heading.fontSize[HeadingElementToSize[as]]};
 	font-weight: ${({ as, bold, theme }): string =>
 		bold === false || (typeof bold === 'undefined' && as === 'h1')
 			? theme.tokens.ref.fontWeight.regular
 			: theme.tokens.ref.fontWeight.bold};
-
+	line-height: ${({ theme }): number =>
+		getLineHeightUnitless(theme.tokens.heading.lineHeight)};
 	color: ${getTextColor()};
 
 	${textAlignCss}
