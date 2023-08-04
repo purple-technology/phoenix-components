17603398741d8d8ab1d5a516d560442c6b5b4761
Vlastimil Skupien <vskupien@email.cz>
2023-08-04 14:45:28 +0200

    fix: package installation problem

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -94,5 +94,9 @@ export default outputs
 		input: `${dist}/types/index.d.ts`,
 		output: [{ file: `${dist}/index.d.ts`, format: 'es' }],
 		plugins: [dts()],
-		external: ['nouislider/dist/nouislider.css', 'tippy.js/dist/tippy.css']
+		external: [
+			'nouislider/dist/nouislider.css',
+			'tippy.js/dist/tippy.css',
+			'react-day-picker/dist/style.css'
+		]
 	})
