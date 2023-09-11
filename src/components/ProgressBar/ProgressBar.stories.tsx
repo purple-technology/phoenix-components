cb275d643cd6123ca4c08be64bbf0e1fa778836f
Adam Štěpánek <stepanek.adam@gmail.com>
2023-09-11 14:14:16 +0200

    feat(ProgressBar): new component

---
+++ src/components/ProgressBar/ProgressBar.stories.tsx
@@ -0,0 +1,22 @@
+import { StoryObj } from '@storybook/react'
+import React from 'react'
+
+import { ColorTheme } from '../../types/Color'
+import { ProgressBar as ProgressBarComponent, ProgressBarProps } from './index'
+
+export default {
+	component: ProgressBarComponent,
+	argTypes: {
+		color: {
+			control: 'radio',
+			options: [undefined, ...ColorTheme]
+		}
+	},
+	args: {
+		value: 50
+	}
+}
+
+export const ProgressBar: StoryObj<ProgressBarProps> = {
+	render: (args) => <ProgressBarComponent {...args} />
+}
---
+++ src/components/ProgressBar/ProgressBar.styles.tsx
@@ -0,0 +1,33 @@
+import styled from 'styled-components'
+
+import { ColorTheme, isColorTheme } from '../../types/Color'
+import { CSSColor } from '../../types/CSSValue'
+import { marginCss } from '../common/Spacing/SpacingStyles'
+
+export const Wrapper = styled.div`
+	background-color: ${({ theme }): string =>
+		theme.tokens.color.background.tertiary};
+	border-radius: ${({ theme }): string => theme.tokens.ref.borderRadius.pill};
+	height: 9px;
+
+	${marginCss};
+`
+
+export interface ProgressBarStyledProps {
+	$value?: number
+	$color?: ColorTheme | CSSColor
+}
+
+export const Progress = styled.div<ProgressBarStyledProps>`
+	background-color: ${({ theme, $color }): string =>
+		!$color
+			? theme.tokens.color.background.brand.primary
+			: isColorTheme($color)
+			? theme.tokens.color.background[$color].primary
+			: $color};
+	border-radius: ${({ theme }): string => theme.tokens.ref.borderRadius.pill};
+	width: ${({ $value }): string => ($value ? `${$value}%` : '0%')};
+	height: 100%;
+	transition: width
+		${({ theme }): string => theme.tokens.duration.transition.base};
+`
