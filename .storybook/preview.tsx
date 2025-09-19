b65b38f7353502315e0127a28274a53561c71b2d
Petr Hendrych <phendrych@purple-technology.com>
2025-09-19 10:25:23 +0200

    feat: excluding margin and padding props to not be forwarded to the DOM

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -6,8 +6,32 @@ import { StyleSheetManager, ThemeProvider } from 'styled-components'
 import { GlobalStyles } from '../src/globalStyles'
 import theme from '../src/theme'
 
+// Define spacing props that should not be forwarded (copying of the MarginProps and PaddingProps
+const spacingProps = new Set([
+	// Margin props
+	'm',
+	'mt',
+	'mr',
+	'mb',
+	'ml',
+	'mx',
+	'my',
+	// Padding props
+	'p',
+	'pt',
+	'pr',
+	'pb',
+	'pl',
+	'px',
+	'py'
+])
+
 // Custom shouldForwardProp function
 const customShouldForwardProp = (prop: string): boolean => {
+	// Don't forward spacing props
+	if (spacingProps.has(prop)) {
+		return false
+	}
 	// Forward all props that don't start with '$' or aren't styled-system props
 	return !prop.startsWith('$') && shouldForwardProp(prop)
 }
